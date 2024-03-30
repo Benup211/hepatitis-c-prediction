6eaf8b6cbacf779af97a4d6aabe545ebeb3580df
benup211 <benup211@gmail.com>
2024-03-30 10:39:38 +0545

    changes:added dictionary to map predicted output

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status
+from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel,Field
 import pickle5 as pickle
@@ -26,13 +27,22 @@ app.mount("/static", StaticFiles(directory=static_folder), name="static")
 @app.get("/")
 def hello():
     return {"message": "This is a Hepatitis C Prediction App"}
-
+finding={
+    0:"Blood Donor",
+    1:"Suspected Blood Donor",
+    2:"Hepatitis,Fibrosis,Cirrhosis"
+}
 @app.post("/")
-def predict(form: FormSchema):
-    model_path = os.path.join(static_folder, "hepatitis_model.pkl")
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
-    value = [form.Age,form.Sex,form.ALB,form.ALP,form.ALT,form.AST,form.BIL,form.CHE,form.CHOL,form.CREA,form.GGT,form.PROT]
-    y_pre = model.predict([value])
-    output = "m" if y_pre[0] == 1 else "f"
-    return {"value": form, "output": output}
+def predict(form: FormSchema,response:Response):
+    try:
+        model_path = os.path.join(static_folder, "hepatitis_model.pkl")
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
+        value = [form.Age,form.Sex,form.ALB,form.ALP,form.ALT,form.AST,form.BIL,form.CHE,form.CHOL,form.CREA,form.GGT,form.PROT]
+        y_pre = model.predict([value])
+        output = finding[y_pre[0]]
+        response.status_code=status.HTTP_200_OK
+        return {"value": form, "output": output}
+    except Exception as E:
+        response.status_code=status.HTTP_400_BAD_REQUEST
+        return {"error":E}
