c1af3f529eae44afb657a6dfe178b1a194cd10cc
benup211 <benup211@gmail.com>
2024-03-31 08:59:11 +0545

    deploy

--- frontend/src/components.jsx/predictForm.jsx
+++ frontend/src/components.jsx/predictForm.jsx
@@ -10,7 +10,7 @@ export const PredictForm = () => {
                 acc[key] = parseInt(value);
                 return acc;
             }, {});
-            const response = await fetch('http://127.0.0.1:8000', {
+            const response = await fetch('https://hepatitis-c-prediction.onrender.com/', {
                 method: 'POST',
                 body: JSON.stringify(parsedData),
                 headers: {
